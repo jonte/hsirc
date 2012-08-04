f575a3395bcc57caa42bbc0d9099e7d0aa57722c
Jonatan Pålsson <jonatan.p@gmail.com>
2012-08-04 13:44:12 +0200

    Added channel and user MODE commands

--- Network/IRC/Commands.hs
+++ Network/IRC/Commands.hs
@@ -11,19 +11,22 @@ module Network.IRC.Commands (
   , quit
   , privmsg
   , kick
+  , channelMode
+  , userMode
   ) where
 
 import Network.IRC.Base
+import Data.Maybe (maybeToList)
 
-type Channel    = String
-type Password   = String
-type Reason     = String
+type Channel     = String
+type Password    = String
+type Reason      = String
+type UserMask    = String
+type ChannelMode = Char
+type UserMode    = Char
 
 mkMessage           :: String -> [Parameter] -> Message
 mkMessage cmd params = Message Nothing cmd params
-
-
-
 
 nick  :: UserName -> Message
 nick u = mkMessage "NICK" [u]
@@ -47,3 +50,11 @@ quit Nothing  = mkMessage "QUIT" []
 
 privmsg    :: String -> String -> Message
 privmsg c m = mkMessage "PRIVMSG" [c,m]
+
+channelMode :: Channel -> [ChannelMode] -> Maybe Int -> 
+                Maybe UserName -> Maybe UserMask -> Message
+channelMode c cm l u um = mkMessage "MODE" params
+    where params = [c, cm] ++ maybeToList (fmap show l) ++ maybeToList u ++ maybeToList um
+
+userMode :: UserName -> [UserMode] -> Message
+userMode n m = mkMessage "MODE" [n,m]
